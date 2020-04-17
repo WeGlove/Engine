1a67e9d7f879aff7bcf33156a6ae22d6082ff03a
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2020-04-18 00:28:59 +0200

    Moved Peripherals and Connection to engine

---
+++ Engine/Peripherals/Mouse.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod
+
+
+class Mouse:
+
+    @abstractmethod
+    def get_position(self):
+        pass
+
+    @abstractmethod
+    def clicked(self):
+        pass
+
+    @abstractmethod
+    def left_clicked(self):
+        pass
+
+    @abstractmethod
+    def middle_clicked(self):
+        pass
+
+    @abstractmethod
+    def right_clicked(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
